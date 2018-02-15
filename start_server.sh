deadb52929a84bb75fac19c2327bfc3a041d03a1
Neil Swainston <neil.swainston@manchester.ac.uk>
2018-02-15 19:22:26 +0000

    Update start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -10,10 +10,8 @@ mkdir selenzyme
 wget http://130.88.113.226/selenzy/selenzy.tar.gz
 tar -xzvf selenzy.tar.gz -C selenzyme
 
-ls -l $DIR/selenzyme
-
 docker build -t selenzyme .
 
 docker run --name nginx-proxy -d -p 80:80 -v /var/run/docker.sock:/tmp/docker.sock:ro jwilder/nginx-proxy
 
-docker run --name selenzyme -d -p :5000 -e LD_LIBRARY_PATH='/opt/conda/bin/../lib' -e VIRTUAL_HOST=selenzyme.synbiochem.co.uk -v=$DIR/selenzyme:/selenzyme selenzyme
+docker run --name selenzyme -d -p :5000 -e LD_LIBRARY_PATH='/opt/conda/bin/../lib' -e VIRTUAL_HOST=selenzyme.synbiochem.co.uk -v $DIR/selenzyme:/selenzyme selenzyme
