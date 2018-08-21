6b9d5730c91095d04c1b3440b493bf780cfdaff0
mibsspc2 <1038612+pablocarb@users.noreply.github.com>
2018-08-21 08:39:39 +0100

    Remove selprom call

--- start_server.sh
+++ start_server.sh
@@ -17,27 +17,3 @@ docker run --name nginx-proxy -d -p 80:80 -p 88:7700 -v /var/run/docker.sock:/tm
 
 docker run --name selenzyme -d -p :5000 -e LD_LIBRARY_PATH='/opt/conda/bin/../lib' -e VIRTUAL_HOST=selenzyme.synbiochem.co.uk -v $DIR/selenzyme:/selenzyme selenzyme
 
-# Selprom
-
-GITLAB=ssh://gitlab@gitlab.cs.man.ac.uk:22222/pablo-carbonell
-GITHUB=https://github.com/synbiochem/selprom.git
-# Run script from its folder
-CWD=$PWD
-# Define repository location
-REPO=$GITLAB/sbc-prom.git
-
-# Clone production server
-rm -rf sbc-prom
-git clone $REPO sbc-prom
-cd sbc-prom
-git checkout -b prod origin/prod
-cd $CWD
-
-# Clone container 
-git clone $GITHUB selprom
-cd selprom
-
-docker build -t selprom .
-
-# Run container with user's uid to avoid permission issues when updating the repository
-docker run -u `id -u $USER` --name selprom -d -p 7700:7700 -e LD_LIBRARY_PATH='/opt/conda/bin/../lib' -v $CWD/sbc-prom:/selprom selprom 
